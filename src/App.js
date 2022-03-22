099ebbe3a56a94a6a02b92dc6168a2a215c26c00
Katrina Lenell Hill <khill365@gmail.com>
2022-03-21 20:49:20 -0700

    added code for error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import axios from 'axios';
 
@@ -7,6 +8,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       cityData: {},
+      error: false,
+      errorMessage:''
     }
   }
 
@@ -15,32 +18,48 @@ class App extends React.Component {
       city: event.target.value
     })
   }
+
   getCityData = async (event) => {
     event.preventDefault();
-    // get the data from the API
-    let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
-    // save that data into state
-    this.setState({
-      cityData: 'data back from API.data.results'
-    });
+    try {
+      // get the data from the API
+      let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+      // save that data into state
+      this.setState({
+        cityData: cityData.data.results
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: `An error has occured: ${error.response.status}`
+      })
+      
+  }
   };
 
   render() {
 
+    let cityDataListItems = this.state.cityData.map((city, index) => <li key={index}>{city.name}</li>);
+    
     return (
+      
       <>
         <h1>Data from API</h1>
         <form onSubmit={this.getCityData}>
           <label>Pick a City:
-            <input type = 'text' onInput={this.handleCityInput}/> 
-            <button>Explor!</button>
+            <input type='text' onInput={this.handleCityInput} />
+            <button>Explore!</button>
           </label>
         </form>
-      
-    
-  
+        {this.state.error
+        ?
+        <p>{this.state.errorMessage}</p>
+        :
+        <ul>
+          {cityDataListItems}
+        </ul>
 
-        {/* function App() {
+        /* function App() {
           return (
             <div className="App">
               <header className="App-header">
@@ -58,8 +77,8 @@ class App extends React.Component {
                 </a>
               </header>
             </div> */}
-          );
-      </>      
+        {/* ); */}
+      </>
     );
   }
 }
