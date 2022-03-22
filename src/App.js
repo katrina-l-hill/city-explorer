bb0939e70f50967f4c1e051659ec574111d6ce4d
Katrina Lenell Hill <khill365@gmail.com>
2022-03-21 21:56:07 -0700

    troubleshooting img error

--- src/App.js
+++ src/App.js
@@ -7,58 +7,57 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cityData: {},
+      cityData: [],
       error: false,
-      errorMessage:''
+      errorMessage: '',
+      map: ''
     }
   }
-
   handleCityInput = (event) => {
     this.setState({
       city: event.target.value
     })
   }
+  //query string format: http://somesite.com?key=value&key2=value&key3=value
 
   getCityData = async (event) => {
     event.preventDefault();
     try {
       // get the data from the API
       let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+      let mapPng = await axios.get(`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=40.7484284,-73.9856546198733&zoom=14`);
       // save that data into state
       this.setState({
-        cityData: cityData.data.results
+        cityData: cityData.data,
+        map: mapPng.data
       });
     } catch (error) {
+      console.log(error);
       this.setState({
         error: true,
         errorMessage: `An error has occured: ${error.response.status}`
-      })
-      
-  }
+      });
+    }
   };
-
   render() {
-
-    let cityDataListItems = this.state.cityData.map((city, index) => <li key={index}>{city.name}</li>);
-    
+    let cityDataListItems = this.state.cityData.map((city, index) => <li key={index}>{city.display_name}</li>);
     return (
-      
       <>
         <h1>Data from API</h1>
         <form onSubmit={this.getCityData}>
           <label>Pick a City:
             <input type='text' onInput={this.handleCityInput} />
-            <button>Explore!</button>
+            <button type="submit">Explore!</button>
           </label>
         </form>
+        <img src={this.state.map} />
         {this.state.error
-        ?
-        <p>{this.state.errorMessage}</p>
-        :
-        <ul>
-          {cityDataListItems}
-        </ul>
-
+          ?
+          <p>{this.state.errorMessage}</p>
+          :
+          <ul>
+            {cityDataListItems}
+          </ul>
         /* function App() {
           return (
             <div className="App">
