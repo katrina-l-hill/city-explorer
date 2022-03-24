0e3a6a4c6978876af7d008450a5894a46283e53d
Katrina Lenell Hill <khill365@gmail.com>
2022-03-23 17:17:53 -0700

    added semicolons, removed trailing spaces

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityData: [],
+      error: false,
+      errorMessage: '',
+      map: ''
+    };
+  }
+  handleCityInput = (event) => {
+    this.setState({
+      city: event.target.value
+    });
+  };
+  //query string format: http://somesite.com?key=value&key2=value&key3=value
+
+  getCityData = async (event) => {
+    event.preventDefault();
+    try {
+      // get the data from the API
+      let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+
+      // save that data into state
+      this.setState({
+        cityData: cityData.data,
+      });
+      console.log(this.state.cityData);
+      let mapPng = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData[0].lat},${this.state.cityData[0].lon}&zoom=14`;
+      console.log(mapPng);
+      this.setState({
+        map: mapPng
+      });
+    } catch (error) {
+      // console.log(error);
+      this.setState({
+        error: true,
+        errorMessage: `An error has occured: ${error.response.status}`
+      });
+    }
+  };
+  render() {
+    // console.log('map render',this.state.map);
+    let cityDataListItems = this.state.cityData.map((city, index) => <li key={index}>{city.display_name}</li>);
+    return (
+      <>
+        <h1>Data from API</h1>
+        <form onSubmit={this.getCityData}>
+          <label>Type the name of a City:
+            <input type='text' onInput={this.handleCityInput} />
+            <button type="submit">Explore!</button>
+          </label>
+        </form>
+        <img src={this.state.map} alt="Map" />
+        {this.state.error
+          ?
+          <p>{this.state.errorMessage}</p>
+          :
+          <ul>
+            {cityDataListItems}
+          </ul>
+        }
+      </>
+    );
+  }
 }
 
 export default App;
